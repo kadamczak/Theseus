a0e2a708474ebd0ac9a317fbb51c9a923683a125
kadamczak <cynder77@wp.pl>
2023-10-04 21:26:55 +0200

    Bring back default constructor to MazeDto

--- Theseus.Infrastructure/Dtos/MazeDto.cs
+++ Theseus.Infrastructure/Dtos/MazeDto.cs
@@ -18,5 +18,13 @@ namespace Theseus.Infrastructure.Dtos
             Width = maze.ColumnAmount;
             Data = cellsAsBytes;
         }
+
+        public MazeDto(Guid? id, int height, int width, byte[] data)
+        {
+            Id = id;
+            Height = height;
+            Width = width;
+            Data = data;
+        }
     }
 }
