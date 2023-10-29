682cce36b4a5a424bf2bbc1c75c5e0a8dfa4ffbb
kadamczak <cynder77@wp.pl>
2023-10-29 21:55:22 +0100

    Improve maze drawing in MazeCommandListView

--- Theseus.WPF/Code/Views/MazeViews/MazeCommandListView.xaml.cs
+++ Theseus.WPF/Code/Views/MazeViews/MazeCommandListView.xaml.cs
@@ -1,8 +1,5 @@
-﻿using System.Collections.Generic;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Media;
-using Theseus.WPF.Code.ViewModels;
 using Theseus.WPF.Code.Views.Components.MazeCanvases;
 
 namespace Theseus.WPF.Code.Views
@@ -12,76 +9,16 @@ namespace Theseus.WPF.Code.Views
     /// </summary>
     public partial class MazeCommandListView : UserControl
     {
-        private bool _mazeCanvasesLoaded = false;
-        private IEnumerable<MazeWithSolutionCanvasView> _mazeWithSolutionViews;
-
         public MazeCommandListView()
         {
             InitializeComponent();
         }
 
-        public void Initialize()
+        private void MazesWithSolutionCanvasView_Loaded(object sender, RoutedEventArgs e)
         {
-            this._mazeWithSolutionViews = GetMazeWithSolutionViews();
-            foreach (var mazeView in _mazeWithSolutionViews)
-            {
-                mazeView.InitializeDataContexts();
-            }
-
-            _mazeCanvasesLoaded = true;
-            DrawMazes();
-        }
-
-
-        private void DrawMazes()
-        {
-            foreach (var mazeView in _mazeWithSolutionViews)
-            {
-                mazeView.DrawScaledMazeWithVisibleSolutionPath(2);
-            }
-        }
-
-        private List<MazeWithSolutionCanvasView> GetMazeWithSolutionViews()
-        {
-            List<MazeWithSolutionCanvasView> mazeCanvasList = new List<MazeWithSolutionCanvasView>();
-
-            var mazesListBox = this.FindName("MazesWithSolutionListBox") as ListBox;
-            for (int i = 0; i < mazesListBox.Items.Count; i++)
-            {
-                ListBoxItem mazeItem = (ListBoxItem)(mazesListBox.ItemContainerGenerator.ContainerFromItem(mazesListBox.Items[i]));
-                ContentPresenter mazeContentPresenter = FindVisualChild<ContentPresenter>(mazeItem);
-                DataTemplate mazeDataTemplate = mazeContentPresenter.ContentTemplate;
-                var mazeCanvas = (MazeWithSolutionCanvasView)mazeDataTemplate.FindName("MazeCanvas", mazeContentPresenter);
-                mazeCanvasList.Add(mazeCanvas);
-            }
-
-            return mazeCanvasList;
-        }
-
-
-        private childItem FindVisualChild<childItem>(DependencyObject obj)
-        where childItem : DependencyObject
-        {
-            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
-            {
-                DependencyObject child = VisualTreeHelper.GetChild(obj, i);
-                if (child != null && child is childItem)
-                {
-                    return (childItem)child;
-                }
-                else
-                {
-                    childItem childOfChild = FindVisualChild<childItem>(child);
-                    if (childOfChild != null)
-                        return childOfChild;
-                }
-            }
-            return null;
-        }
-
-        private void MazesWithSolutionListBox_Loaded(object sender, RoutedEventArgs e)
-        {
-            Initialize();
+            MazeWithSolutionCanvasView mazeCanvas = sender as MazeWithSolutionCanvasView;
+            mazeCanvas.InitializeDataContexts();
+            mazeCanvas.DrawScaledMazeWithVisibleSolutionPath(2);
         }
     }
 }
