044ffb0643ea9fa42575f77be0ab0032c3614ad4
kadamczak <cynder77@wp.pl>
2023-12-05 00:53:32 +0100

    Update end exam page

--- Theseus.WPF/Code/ViewModels/ExamViewModels/ExamEndViewModel.cs
+++ Theseus.WPF/Code/ViewModels/ExamViewModels/ExamEndViewModel.cs
@@ -1,12 +1,17 @@
-﻿using Theseus.WPF.Code.Bases;
+﻿using System.Windows.Input;
+using Theseus.WPF.Code.Bases;
+using Theseus.WPF.Code.Commands.NavigationCommands;
+using Theseus.WPF.Code.Services;
 
 namespace Theseus.WPF.Code.ViewModels
 {
     public class ExamEndViewModel : ViewModelBase
     {
+        public ICommand GoBack { get; }
 
-        public ExamEndViewModel()
+        public ExamEndViewModel(NavigationService<BeginTestViewModel> navigationService)
         {          
+            GoBack = new NavigateCommand<BeginTestViewModel>(navigationService);
         }
     }
 }
