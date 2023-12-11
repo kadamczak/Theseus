b7791cc26f4cba256fbbf729307e576e5ade8cb0
kadamczak <cynder77@wp.pl>
2023-12-11 23:42:24 +0100

    Add enum translation

---
+++ Theseus.WPF/Code/CustomUI/CustomConverters/NullableIntConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace Theseus.WPF.Code.CustomUI.CustomConverters
+{
+    public class NullableIntConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null)
+                return (string)App.Current.Resources["Undisclosed"];
+
+            return value.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Theseus.WPF/Code/CustomUI/CustomConverters/StringToResourceConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace Theseus.WPF.Code.CustomUI.CustomConverters
+{
+    public class StringToResourceConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is null)
+                return (string)App.Current.Resources["Undisclosed"];
+
+            return (string)App.Current.Resources[value.ToString()];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
