b2b7da7b3123e6e728ac656918ee21e0753c8498
kadamczak <cynder77@wp.pl>
2023-12-20 14:35:11 +0100

    Add icon credits

---
+++ Theseus.WPF/Code/Commands/SettingsCommands/ViewCreditsCommand.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Threading.Tasks;
+using Theseus.WPF.Code.Bases;
+
+namespace Theseus.WPF.Code.Commands.SettingsCommands
+{
+    public class ViewCreditsCommand : AsyncCommandBase
+    {
+        public override async Task ExecuteAsync(object parameter)
+        {
+            System.Diagnostics.Process.Start(new ProcessStartInfo
+            {
+                FileName = "index.html",
+                UseShellExecute = true
+            });
+        }
+    }
+}
--- Theseus.WPF/Code/ViewModels/FromNavbar/HomeViewModel.cs
+++ Theseus.WPF/Code/ViewModels/FromNavbar/HomeViewModel.cs
@@ -1,8 +1,16 @@
-﻿using Theseus.WPF.Code.Bases;
+﻿using System.Windows.Input;
+using Theseus.WPF.Code.Bases;
+using Theseus.WPF.Code.Commands.SettingsCommands;
 
 namespace Theseus.WPF.Code.ViewModels
 {
     public class HomeViewModel : ViewModelBase
     {
+        public ICommand ViewCredits { get; }
+
+        public HomeViewModel()
+        {
+            ViewCredits = new ViewCreditsCommand();
+        }
     }
 }
