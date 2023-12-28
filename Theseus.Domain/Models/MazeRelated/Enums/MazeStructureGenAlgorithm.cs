bd65efe336d2ebf93d162f8add7899a8413d0670
kadamczak <cynder77@wp.pl>
2023-12-28 21:23:47 +0100

    Swap Kruskal algorithm and Prim algorithm usage in exam set generation

--- Theseus.Domain/Models/MazeRelated/Enums/MazeStructureGenAlgorithm.cs
+++ Theseus.Domain/Models/MazeRelated/Enums/MazeStructureGenAlgorithm.cs
@@ -7,8 +7,8 @@
     {
         AldousBroder = 0,
         HuntAndKill = 1,
-        Kruskal = 2,
-        TruePrim = 3,
+        Kruskal = 3,
+        TruePrim = 2,
         Binary = 5,
         Sidewinder = 4,
     }
