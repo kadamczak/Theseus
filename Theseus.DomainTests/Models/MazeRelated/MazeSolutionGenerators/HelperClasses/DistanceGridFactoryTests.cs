0b07c4612d3f0d7711443147413c61ea053b5400
kadamczak <cynder77@wp.pl>
2024-01-15 18:43:29 +0100

    Maze generator and distance grid factory tests

---
+++ Theseus.DomainTests/Models/MazeRelated/MazeSolutionGenerators/HelperClasses/DistanceGridFactoryTests.cs
@@ -0,0 +1,57 @@
+﻿using Theseus.Domain.Models.MazeRelated.Enums;
+using Theseus.Domain.Models.MazeRelated.MazeGenerators;
+using Theseus.Domain.Models.MazeRelated.MazeRepresentation;
+using Xunit;
+
+namespace Theseus.Domain.Models.MazeRelated.MazeSolutionGenerators.HelperClasses.Tests
+{
+    public class DistanceGridFactoryTests
+    {
+        public static IEnumerable<object[]> GenerateMazeStructure_Data => new List<object[]>
+            {
+                new object[] { new Maze(8, 12), 44, (0, 0), MazeStructureGenAlgorithm.AldousBroder, new Dictionary<(int row, int col), int>()
+                                                                                                    {
+                                                                                                        {(0, 1), 1},
+                                                                                                        {(0, 0), 0},
+                                                                                                        {(1, 0), 5},
+                                                                                                        {(3, 1), 8},
+                                                                                                        {(4, 3), 15},
+                                                                                                        {(6, 11), 51},
+                                                                                                        {(6, 6), 54},
+                                                                                                    } },
+                new object[] { new Maze(9, 7), 42, (5, 4), MazeStructureGenAlgorithm.HuntAndKill, new Dictionary<(int row, int col), int>()
+                                                                                                    {
+                                                                                                        {(8, 1), 12},
+                                                                                                        {(8, 2), 5},
+                                                                                                        {(0, 3), 10},
+                                                                                                        {(7, 3), 17}
+                                                                                                    } }
+            };
+
+        [Theory]
+        [MemberData(nameof(GenerateMazeStructure_Data))]
+        public void CreateDistanceGrid_ShouldCreateDistanceGrid(Maze maze,
+                                                                int randomSeed,
+                                                                (int rowIndex, int colIndex) rootCellCoordinates,
+                                                                MazeStructureGenAlgorithm mazeStructureAlgorithm,
+                                                                Dictionary<(int row, int col), int> expectedDistancesForCells)
+        {
+            //arrange
+            var generatorFactory = new MazeStructureGeneratorFactory();
+            var generator = generatorFactory.Create(mazeStructureAlgorithm);
+            generator.GenerateMazeStructureInGrid(maze, randomSeed);
+
+            DistanceGridFactory gridFactory = new DistanceGridFactory();
+
+            //act
+            var distanceGrid = gridFactory.CreateDistanceGrid(maze.GetCell(rootCellCoordinates)!);
+
+            //assert
+            foreach (var example in expectedDistancesForCells)
+            {
+                var cell = maze.GetCell(example.Key)!;
+                Assert.Equal(example.Value, distanceGrid.Distance[cell]);
+            }
+        }
+    }
+}
---
+++ Theseus.DomainTests/Models/MazeRelated/MazeStructureGenerators/Implementations/SidewinderMazeStructureGeneratorTests.cs
@@ -0,0 +1,41 @@
+﻿using Theseus.Domain.Models.MazeRelated.Enums;
+using Theseus.Domain.Models.MazeRelated.MazeRepresentation;
+using Xunit;
+
+namespace Theseus.Domain.Models.MazeRelated.MazeStructureGenerators.Implementations.Tests
+{
+    public class SidewinderMazeStructureGeneratorTests
+    {
+        public static IEnumerable<object[]> GenerateMazeStructure_Data => new List<object[]>
+            {
+                new object[] { new Maze(8, 12), 44, (0, 0), new List<Direction>() { Direction.East, Direction.South } },
+                new object[] { new Maze(8, 12), 44, (6, 3), new List<Direction>() { Direction.East, Direction.South } },
+                new object[] { new Maze(8, 12), 44, (6, 9), new List<Direction>() { Direction.West, Direction.East, Direction.North } },
+                new object[] { new Maze(8, 12), 44, (7, 11), new List<Direction>() { Direction.North } },
+                new object[] { new Maze(8, 12), 44, (0, 6), new List<Direction>() { Direction.West, Direction.East, Direction.South } },
+
+                new object[] { new Maze(5, 3), 40, (3, 1), new List<Direction>() { Direction.East, Direction.North, Direction.South } },
+                new object[] { new Maze(5, 3), 40, (0, 2), new List<Direction>() { Direction.West } },
+                new object[] { new Maze(5, 3), 40, (2, 1), new List<Direction>() { Direction.North, Direction.South } },
+            };
+
+        [Theory]
+        [MemberData(nameof(GenerateMazeStructure_Data))]
+        public void GenerateMazeStructureInGrid_ShouldGenerateMaze(Maze maze,
+                                                                   int randomSeed,
+                                                                   (int rowIndex, int colIndex) testedCellCoordinates,
+                                                                   List<Direction> expectedDirectionsOfAllLinkedNeighbours)
+        {
+            //arrange
+            Cell testedCell = maze.GetCell(testedCellCoordinates)!;
+            var generator = new SidewinderMazeStructureGenerator();
+
+            //act
+            generator.GenerateMazeStructureInGrid(maze, randomSeed);
+
+            //assert
+            var directionsOfAllLinkedNeighbours = testedCell.GetLinkedCells().Select(c => testedCell.GetNeighbourDirection(c));
+            Assert.True(Enumerable.SequenceEqual(expectedDirectionsOfAllLinkedNeighbours, directionsOfAllLinkedNeighbours));
+        }
+    }
+}
